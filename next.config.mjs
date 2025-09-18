5dc23fb9552c17e8dac9595ca86a2d0bb0052d51
GreyberSojo <greybersojo@gmail.com>
2025-09-18 18:40:22 -0300

    fix some css/js issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 // next.config.mjs
 const isProd = process.env.NODE_ENV === 'production';
-const repo = 'greyber-portfolio'; // 👈 your repo slug
+const repo = 'Greyber-Portfolio';
 
 export default {
-  output: 'export',              // produce static ./out
-  images: { unoptimized: true }, // next/image on static hosts
-  basePath: isProd ? `/${repo}` : undefined,   // needed for project pages
+  output: 'export',             
+  images: { unoptimized: true }, 
+  basePath: isProd ? `/${repo}` : undefined, 
   assetPrefix: isProd ? `/${repo}/` : undefined,
-  // Optional:
-  // trailingSlash: true, // if you prefer /about/ style URLs
-  eslint: { ignoreDuringBuilds: false }, // or true to skip ESLint on CI
+  eslint: { ignoreDuringBuilds: false }, 
 };
